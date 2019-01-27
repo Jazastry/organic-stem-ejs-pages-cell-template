6477b1a5dad6503c328ce977c543d55d1d2e095f
Jazastry <jazastry@gmail.com>
2019-01-27 14:54:36 +0200

    updates index.js file

--- index.js
+++ index.js
@@ -3,22 +3,23 @@
 const StackUpgrade = require('organic-stack-upgrade')
 const path = require('path')
 
+
 const execute = async function ({destDir = process.cwd(), answers} = {}) {
   let stack = new StackUpgrade({
     destDir: destDir,
     packagejson: path.join(__dirname, '/package.json')
   })
-  let resulted_answers = await stack.configureMergeAndUpdateJSON({
-    sourceDir: path.join(__dirname, 'seed'),
-    answers
-  })
   let coreTemplateExists = false
   try {
-    coreTemplateExists = await stack.checkUpgrade('organic-stem-core-template', '^1.0.0')
+    coreTemplateExists = await stack.checkUpgrade('organic-stem-core-template', '^2.1.0')
   } catch (e) {
     // ignore any errors
     coreTemplateExists = false
   }
+  let resulted_answers = await stack.configureMergeAndUpdateJSON({
+    sourceDir: path.join(__dirname, 'seed'),
+    answers
+  })
   if (!coreTemplateExists) {
     console.warn('organic-stem-core-template not found...')
     console.info('force installing organic-angel, angelscripts-help and angelscripts-monorepo')
@@ -28,7 +29,6 @@ const execute = async function ({destDir = process.cwd(), answers} = {}) {
   console.info(`run npm install on ${cellName}...`)
   await stack.exec(`npx angel repo cell ${cellName} -- npm install`)
 }
-
 if (module.parent) {
   module.exports = execute
 } else {
